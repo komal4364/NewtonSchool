730325cd7cb4b2abfead0e3c72e2eb96276e6591
Komal Kumar Seelam <seelam436@gmail.com>
2021-08-10 22:54:00 +0530

    week2 changes

---
+++ week2/day1/Swap.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class Swap {
+    public static void main(String[] args) {
+        int []arr = {2, 1, 5, 3};
+        System.out.println("Before Swap: " + Arrays.toString(arr));
+        swap(arr, 0, 1);
+        swap(arr, 2, 3);
+        System.out.println("After Swap: " + Arrays.toString(arr));
+        String []strArr = {"world", "hello"};
+        swapString(strArr, 0, 1);
+        System.out.println("After String Swap: " + Arrays.toString(strArr));
+    }
+
+    private static void swapString(String []arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    //modular function;
+    private static void swap(int []arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
